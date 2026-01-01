4bd53bb76b377d3fcceb9293ba213629b147e821
iimuz <iz.primary.story+github@gmail.com>
2026-01-01 06:04:09 +0000

    style: :lipstick: eslint の環境設定

--- awscdk/assets/functions/basic-auth.js
+++ awscdk/assets/functions/basic-auth.js
@@ -1,7 +1,8 @@
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 function handler(event) {
   var request = event.request;
   var authHeaders = request.headers.authorization;
-  var expected = "Basic " + "dXNlcjpwYXNz"; // user:pass base64 encoded
+  var expected = 'Basic ' + 'dXNlcjpwYXNz'; // user:pass base64 encoded
 
   if (authHeaders && authHeaders.value === expected) {
     return request;
@@ -9,9 +10,9 @@ function handler(event) {
 
   return {
     statusCode: 401,
-    statusDescription: "Unauthorized",
+    statusDescription: 'Unauthorized',
     headers: {
-      "www-authenticate": { value: "Basic realm=\"Access to Console\"" }
-    }
+      'www-authenticate': { value: 'Basic realm="Access to Console"' },
+    },
   };
 }
